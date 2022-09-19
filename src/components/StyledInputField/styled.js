9772e415585f7a4dc391cb9ff1daef993b4bb127
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-19 09:54:53 +0200

    add projectlist

---
+++ src/components/StyledInputField/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledInputField = styled.input`
+	width: 250px;
+	margin: 10px;
+	padding: 5px 10px;
+`;
+
+export default StyledInputField;
---
+++ src/components/StyledProjectName/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledProjectName = styled.li`
+	display: flex;
+	align-items: center;
+	width: 300px;
+	height: 30px;
+	padding: 20px;
+	border: 2px solid black;
+	list-style: none;
+`;
+
+export default StyledProjectName;
