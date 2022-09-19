1be6c0b723bfa457d3943bf1291720f1fa4ced98
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-19 13:03:37 +0200

    fix add button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 
+import StyledAddButton from '../StyledAddButton/styled';
 import StyledNavbar from '../StyledNavbar/styled';
 
 export default function Navbar() {
 	return (
 		<StyledNavbar>
 			<Link href="/">Projects</Link>
-			<Link href="/newproject">+</Link>
+			<Link href="/newproject">
+				<a>
+					<StyledAddButton>+</StyledAddButton>
+				</a>
+			</Link>
 			<Link href="/profile">Profile</Link>
 		</StyledNavbar>
 	);
--- src/components/StyledAddButton/styled.js
+++ src/components/StyledAddButton/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledAddButton = styled.button`
 	position: fixed;
 	bottom: 30px;
+	left: 130px;
 	padding: 20px;
 	border: none;
 	border-radius: 50px;
