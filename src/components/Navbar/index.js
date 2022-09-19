757e6c4b214cfb2f03f82b8148bf44b3a1218fd9
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-19 12:57:26 +0200

    fix deployment error

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,12 @@
 import Link from 'next/Link';
-/* import {useRouter} from 'next/router'; */
 
-import StyledAddButton from '../StyledAddButton/styled';
 import StyledNavbar from '../StyledNavbar/styled';
 
 export default function Navbar() {
 	return (
 		<StyledNavbar>
 			<Link href="/">Projects</Link>
-			<Link href="/newproject">
-				<a>
-					<StyledAddButton>+</StyledAddButton>
-				</a>
-			</Link>
+			<Link href="/newproject">+</Link>
 			<Link href="/profile">Profile</Link>
 		</StyledNavbar>
 	);
